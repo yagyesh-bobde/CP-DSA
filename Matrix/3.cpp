3915471cb0eaaab36e4ec1ba1d86b383d2e59cd6
Yagyesh Bobde <90238742+yagyesh-bobde@users.noreply.github.com>
2024-03-13 11:37:15 +0000

    col wise

---
+++ Matrix/3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 4;
+    int arr[n][n] = {{1, 2, 3, 4},
+                     {5, 6, 7, 8},
+                     {9, 10, 11, 12},
+                     {13, 14, 15, 16}};
+
+    // alternate way to print array
+
+    int alternate = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[j][i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
